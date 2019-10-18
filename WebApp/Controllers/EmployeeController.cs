99a5f80cd830437ae6087d426a7adfa426bf51a6
Jitendra Kumar <jitendra.sitecore@gmail.com>
2019-10-18 22:42:51 +0530

    change folder strucutre
    change folder strucutre

---
+++ WebApp/Controllers/EmployeeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View(GetEmployees());
+        }
+
+        private List<EmployeeViewModel> GetEmployees()
+        {
+            List<EmployeeViewModel> employees = new List<EmployeeViewModel>();
+
+            for (int index = 1; index <= 15; index++)
+            {
+                employees.Add(new EmployeeViewModel()
+                {
+                    EmployeeCode = "100000" + index,
+                    Name = "Employee-" + index,
+                    Address = "Address-" + index,
+                    Department = "Department-" + index,
+                    Title = "Title-" + index,
+                    ContactNumber = "ContactNumber-" + index
+                });
+            }
+
+            return employees;
+        }
+    }
+}
